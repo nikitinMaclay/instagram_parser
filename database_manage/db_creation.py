52f20b7a424a0eecf968d1bb8d23bd9b755aca8e
nikitinMaclay <guicokiuytr@gmail.com>
2024-03-29 10:29:52 +0300

    gitignore changes

--- database_manage/db_creation.py
+++ database_manage/db_creation.py
@@ -7,7 +7,7 @@ def create_database_local_connection():
         host="localhost",
         port=3306,
         user="root",
-        passwd="Lapa2174",
+        passwd="lapa21",
         database="Instagram",
     )
 
